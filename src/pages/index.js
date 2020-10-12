559517d90195baf9f094ad189d13685476c647c2
Yazid <yazid@MacBook-Air-de-Yazid.local>
2020-10-12 19:42:10 +0200

    version 2.0.5

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,19 +28,27 @@ import Splash from "../components/sections/splash"
 
 class IndexPage extends Component{
   componentDidMount() {
-    ReactGA.initialize('G-HMDQ0W9P6M');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    // ReactGA.initialize('G-HMDQ0W9P6M');
+    // ReactGA.pageview(window.location.pathname + window.location.search);
 
-    // !function(f,b,e,v,n,t,s)
-    // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    // n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    // if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    // n.queue=[];t=b.createElement(e);t.async=!0;
-    // t.src=v;s=b.getElementsByTagName(e)[0];
-    // s.parentNode.insertBefore(t,s)}(window, document,'script',
-    // 'https://connect.facebook.net/en_US/fbevents.js');
-    // fbq('init', '326698568627198');
-    // fbq('track', 'PageView');
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HMDQ0W9P6M"></script>
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HMDQ0W9P6M');
+
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '326698568627198');
+    fbq('track', 'PageView');
   }
   render() {
     return (
