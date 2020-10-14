e2b59896b8b674ef449861ecad9f29454eecb28a
Yazid <yazid@MacBook-Air-de-Yazid.local>
2020-10-14 15:53:37 +0200

    version 2.1.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ import { JsonLd } from "react-schemaorg";
 
 class IndexPage extends Component {
   componentDidMount() {
-    ReactGA.initialize('UA-180544186-1', { debug: false });
+    ReactGA.initialize('UA-180544186-1', { debug: true });
     ReactGA.pageview(window.location.href + window.location.search);
 
 
